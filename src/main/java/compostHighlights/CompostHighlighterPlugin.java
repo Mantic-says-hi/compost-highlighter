7e4460ef21c1aeb47fbf2f6fe2bf7266cebd70d1
Mantic <matt.matar25@gmail.com>
2022-07-07 15:50:32 +0800

    Added CompostedOverlay class

--- src/main/java/compostHighlights/CompostHighlighterPlugin.java
+++ src/main/java/compostHighlights/CompostHighlighterPlugin.java
@@ -1,16 +1,25 @@
 package compostHighlights;
 
 import com.google.inject.Provides;
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Inject;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.Tile;
+import net.runelite.api.TileObject;
+import net.runelite.api.events.GameObjectChanged;
+import net.runelite.api.events.GameObjectDespawned;
+import net.runelite.api.events.GameObjectSpawned;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
 @Slf4j
 @PluginDescriptor(
@@ -18,22 +27,33 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class CompostHighlighterPlugin extends Plugin
 {
+
+	@Getter
+	private final Map<TileObject, Tile> patchTiles = new HashMap<>();
 	@Inject
 	private Client client;
 
 	@Inject
 	private CompostHighlighterConfig config;
 
+	@Inject
+	private CompostedOverlay compostedOverlay;
+
+	@Inject
+	private OverlayManager overlayManager;
+
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.info("Compost Highlighter started!");
+		overlayManager.add(compostedOverlay);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		log.info("Compost Highlighter stopped!");
+		overlayManager.remove(compostedOverlay);
 	}
 
 	@Subscribe
@@ -50,4 +70,6 @@ public class CompostHighlighterPlugin extends Plugin
 	{
 		return configManager.getConfig(CompostHighlighterConfig.class);
 	}
+
+
 }
--- src/main/java/compostHighlights/CompostedOverlay.java
+++ src/main/java/compostHighlights/CompostedOverlay.java
@@ -1,2 +1,70 @@
-package compostHighlights;public class CompostedOverlay {
+package compostHighlights;
+
+import static java.awt.Color.GREEN;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import javax.inject.Inject;
+import net.runelite.api.Animation;
+import net.runelite.api.Client;
+import net.runelite.api.Constants;
+import net.runelite.api.DynamicObject;
+import net.runelite.api.GameObject;
+import net.runelite.api.ObjectComposition;
+import net.runelite.api.Player;
+import net.runelite.api.Scene;
+import net.runelite.api.Tile;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.client.plugins.skillcalculator.skills.FarmingAction;
+import net.runelite.client.ui.overlay.Overlay;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.OverlayUtil;
+
+public class CompostedOverlay extends Overlay {
+
+    private final Client client;
+
+    private final CompostHighlighterPlugin plugin;
+
+    private final CompostHighlighterConfig config;
+
+    @Inject
+    private CompostedOverlay(Client client, CompostHighlighterPlugin plugin,CompostHighlighterConfig config)
+    {
+        super(plugin);
+        setPosition(OverlayPosition.DYNAMIC);
+        setLayer(OverlayLayer.ABOVE_SCENE);
+        this.client = client;
+        this.plugin = plugin;
+        this.config = config;
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics)
+    {
+        Player player = client.getLocalPlayer();
+
+        Scene scene = client.getScene();
+        Tile[][][] tiles = scene.getTiles();
+
+        int z = client.getPlane();
+        for (int x = 0; x < Constants.SCENE_SIZE; ++x) {
+            for (int y = 0; y < Constants.SCENE_SIZE; ++y) {
+                Tile tile = tiles[z][x][y];
+
+                GameObject[] gameObjects = tile.getGameObjects();
+                if (gameObjects != null) {
+                    for (GameObject gameObject : gameObjects) {
+                        if (gameObject != null && gameObject.getId() == 8551) {
+
+                            OverlayUtil.renderTileOverlay(graphics, gameObject, "", GREEN);
+                        }
+
+                    }
+                }
+            }
+        }
+
+                return null;
+    }
 }
